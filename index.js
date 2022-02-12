be333e4edbe086b1bc2539d6c8d6ae3d15bfcce1
Utkarsh ojha <utkicon2021@gmail.com>
2022-02-12 18:14:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'utkicon2021@gmail.com',
+    pass: 'Utkarsh.hsraktU18'
+  }
+});
+
+var mailOptions = {
+  from: 'utkicon2021@gmail.com',
+  to: 'utk18sept@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
